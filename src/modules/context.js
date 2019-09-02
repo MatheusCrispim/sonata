2924356168d6f0728547b6ec614b9681417dcf44
ciacelo <marcelo.martins@dce.ufpb.br>
2019-09-02 13:56:12 -0300

    Add images

--- src/modules/context.js
+++ src/modules/context.js
@@ -7,113 +7,12 @@ export default class Context extends Component {
 
     render() {
         return (
-            <View style={styles.container}>
-                <View style={styles.containerTitle}>
-                    <Text style={styles.title}>Escolha onde você quer jogar ?</Text>
-                </View>
-                <View style={styles.firstLine}>
-                    <TouchableOpacity style={{marginRight: 'auto'}} onPress={() => this.props.navigation.navigate('Game')}>
-                        <View style={styles.firstSquary}>
-                            <Text>Animais</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{marginLeft: 'auto'}} onPress={() => alert('Casa')}>
-                        <View style={styles.secondSquary}>
-                            <Text>Casa</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-
-                <View style={styles.secondLine}>
-                    <TouchableOpacity style={{marginRight: 'auto'}} onPress={() => alert('Escola')}>
-                        <View style={styles.thirdSquary}>
-                            <Text>Escola</Text>
-                        </View>
-                    </TouchableOpacity>
-                    <TouchableOpacity style={{marginLeft: 'auto'}} onPress={() => alert('Frutas')}>
-                        <View style={styles.fourthSquare}>
-                            <Text>Frutas</Text>
-                        </View>
-                    </TouchableOpacity>
-                </View>
-            </View>
+            
         )
     }
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#50B3DD',
-        justifyContent: 'center',
-    },
-
-    containerTitle: {
-        alignItems: 'center',
-        marginBottom: 30,
-    },
-
-    title: {
-        fontSize: 35,
-        color: '#fff',
-        fontWeight: 'bold',
-        marginHorizontal: 20,
-        textAlign: 'center',
-    },
-
-    firstLine: {
-        flexDirection: "row",
-        margin: 20,
-    },
-
-    secondLine: {
-        flexDirection: "row",
-        margin: 20,
-    },
-
-    firstSquary: {
-        borderRadius: 20,
-        backgroundColor: '#fff',
-        borderWidth: 5,
-        borderColor: '#86D5FE',
-        width: 140,
-        height: 120,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    secondSquary: {
-        borderRadius: 20,
-        backgroundColor: '#fff',
-        borderWidth: 5,
-        borderColor: '#86D5FE',
-        width: 140,
-        height: 120,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    thirdSquary: {
-        borderRadius: 20,
-        backgroundColor: '#fff',
-        borderWidth: 5,
-        borderColor: '#86D5FE',
-        width: 140,
-        height: 120,
-        justifyContent: 'center',
-        alignItems: 'center'
-    },
-
-    fourthSquare: {
-        borderRadius: 20,
-        backgroundColor: '#fff',
-        borderWidth: 5,
-        borderColor: '#86D5FE',
-        width: 140,
-        height: 120,
-        justifyContent: 'center',
-        alignItems: 'center'
-    }
-
+    
 
 })
